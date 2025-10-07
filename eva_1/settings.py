e1ee9721a42648374b1f38ee43fa1a6e3dc065fc
Momistico <skmomistico@gmail.com>
2025-10-06 21:47:55 -0300

    Conexion BD SQlite

--- eva_1/settings.py
+++ eva_1/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'widget_tweaks',
     "catalog",
 ]
 
@@ -52,10 +53,11 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'eva_1.urls'
 
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
